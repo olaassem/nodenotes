93c9d80241d145764ffcf10bcef406083cf57639
Ola Assem <o.assem@gmail.com>
2018-08-06 13:33:43 +0200

    working with json exs

--- playground/json.js
+++ playground/json.js
@@ -3,3 +3,21 @@
 // let person = JSON.parse(personString);
 // console.log(typeof person);
 // console.log(person);
+
+
+let fs = require('fs');
+
+let originalNote = {
+  title: "Some Title",
+  body: "Some Body"
+}
+
+let originalNoteString = JSON.stringify(originalNote);
+console.log(originalNoteString);
+
+fs.writeFileSync('notes.json', originalNoteString);
+
+let noteString = fs.readFileSync('notes.json');
+let note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
